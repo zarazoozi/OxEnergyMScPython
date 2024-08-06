c63dbb69f5c187d392cb466867ab9080c0d47f28
Scot Wheeler <scot.wheeler@eng.ox.ac.uk>
2024-08-06 11:55:18 +0100

    pre-course tutorials

---
+++ PreCourse Material/Spyder_tutorial_prep.py
@@ -0,0 +1,59 @@
+
+"""
+This section is used for placing comment blocks
+
+@author: PLACE YOUR NAME HERE
+"""
+
+
+# Welcome to your first Spyder file!
+# Follow the tutorial to learn
+# more about this coding environment
+
+
+# Here is a sample line of code where we DEFINE a variable:
+test_variable = 2 + 2
+
+# Run the line above and see what happens in each
+# display window (try figuring out how to run that individual line)
+
+# Run the following now:
+test_variable
+
+# Sometimes Spyder will warn you about errors
+# or ways to improve your script.
+
+# E.g.:
+test_variable = 2 +
+
+# Hover over the x symbol
+
+# Properly commenting on your code is vital!
+# For instance, would you easily know what is being done below?:
+
+MWhpyr = [0];
+MWhpyr[0] = 2*24*365;
+
+# With inefficient code like the lines above, commenting helps with 
+# optimisation, particularly within research groups
+
+
+"""
+Challenge (complete and submit entire Spyder file)
+"""
+
+# Let's get a bit deeper with lists. Here is what you will need to show in
+# in your code below (well commented!):
+    
+#   1) Create a Python 'list' of 10 renewable energy fuel sources (wind, solar etc)
+#   2) Use that list variable to 'print' the 4th fuel type
+#   3) Assign the 5th fuel type to a variable called "fifth"
+#   4) 'print' the statement "The 5th fuel type in the created list is 
+#      <insert fuel type> and the fuel type with the most characters is
+#      <insert fuel type>".
+
+"""
+Number 4 is going to be the most challenging and will require a bit of Googling
+but ... that's the point. Coding involves a LOT of research and this will be 
+a good skill to have. Sites like Stack Overflow will become your best friend!
+"""
